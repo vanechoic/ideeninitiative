09e32e1fad4e5926e667dc0c89c47209f2429b03
vanechoic <34001779+vanechoic@users.noreply.github.com>
2020-04-16 20:34:59 +0200

    initial commit

---
+++ src/test/java/awe/ideeninitiative/IdeeninitiativeApplicationTests.java
@@ -0,0 +1,13 @@
+package awe.ideeninitiative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeeninitiativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
