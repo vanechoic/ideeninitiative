13b8ee0e9b19c59b9ae13741ae04a223d8d23d3f
Nils Jürgens <nils.juergens@edu.fhdw.de>
2020-05-12 16:11:36 +0200

    Startseite dynamischer

--- frontend/src/views/Startseite.vue
+++ frontend/src/views/Startseite.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="startseite">
-     <IdeeBearbeiten msg=""/>
+     <component v-bind:is="component"></component>
   </div>
 </template>
 
 <script>
-//import Hauptseite from '@/components/Hauptseite.vue'
-//import MeineIdeen from '@/components/MeineIdeen.vue'
-//import NeueIdee from '@/components/NeueIdee.vue'
+import Hauptseite from '@/components/Hauptseite.vue'
+import MeineIdeen from '@/components/MeineIdeen.vue'
+import NeueIdee from '@/components/NeueIdee.vue'
 import IdeeBearbeiten from '@/components/IdeeBearbeiten.vue'
 
 export default {
   name: 'Startseite',
   components: {
-    IdeeBearbeiten
+    "hs": Hauptseite,
+    "mi": MeineIdeen,
+    "ni": NeueIdee,
+    "ib": IdeeBearbeiten 
+  },
+  data(){
+    return{
+      component:'hs'
+    }
   }
 }
 </script>
