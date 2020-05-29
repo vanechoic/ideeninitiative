198350101941ff96a055db243ce66a0115872d17
Vanessa <34001779+vanechoic@users.noreply.github.com>
2020-05-29 12:34:13 +0200

    Update IdeeLoeschenServiceTest.java

--- backend/src/test/java/awe/ideeninitiative/restapi/service/IdeeLoeschenServiceTest.java
+++ backend/src/test/java/awe/ideeninitiative/restapi/service/IdeeLoeschenServiceTest.java
@@ -20,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.data.domain.Example;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
