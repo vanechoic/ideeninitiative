48030bf4b1a48b320532cdfec98087526a353c37
vanechoic <vanechoic@web.de>
2020-06-02 14:54:29 +0200

    CSS fuer IdeeBearbeiten komplett

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -46,4 +46,20 @@ select{
   box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4), 0 -1px 1px #fff, 0 1px 0 #fff;
   border-radius: 20px;
 }
+//Scrollbar
+::-webkit-scrollbar {
+  width: 8px;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
