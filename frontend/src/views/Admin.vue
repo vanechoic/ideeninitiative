b8232b84cc9eac268c4153e1329dc09a963f0afb
Nils Jürgens <nils.juergens@edu.fhdw.de>
2020-06-02 10:55:49 +0200

    Admin ohne css

---
+++ frontend/src/views/Admin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="admin">
+    <Admin msg=""/>
+  </div>
+</template>
+
+<script>
+import Admin from '@/components/Admin.vue'
+
+export default {
+  name: 'admin',
+  components: {
+    Admin
+  }
+}
+</script>
+
+<style lang="scss">
+  .admin{
+    height: 70%;
+    margin: auto;
+  }
+</style>
