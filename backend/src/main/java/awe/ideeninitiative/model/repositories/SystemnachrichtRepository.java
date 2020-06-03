f6af8364bd92493bdf46baf35d64283b0ccbc8cf
Vanessa <34001779+vanechoic@users.noreply.github.com>
2020-06-03 10:52:39 +0200

    Frontend admin (#33)
    * Auto stash before merge of "frontend-idee-bearbeiten" and "origin/frontend-idee-bearbeiten"
    
    Admin weiter
    
    * Nutzerverwaltung implementiert
    
    * Endpunkte Systemnachrichten laden und loeschen
    
    * Systemnachrichten anzeigen/lesen/löschen
    
    * Pipes
    
    * Feedback bei Systemnachrichten
    
    Co-authored-by: Nils Jürgens <nils.juergens@edu.fhdw.de>
    Co-authored-by: Jakob <jakob@jbcommerce.de>

--- backend/src/main/java/awe/ideeninitiative/model/repositories/SystemnachrichtRepository.java
+++ backend/src/main/java/awe/ideeninitiative/model/repositories/SystemnachrichtRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SystemnachrichtRepository extends JpaRepository<Nachricht, Long> {
 
     public List<Nachricht> findAllByTitel(String titel);
+
+    public Optional<Nachricht> findFirstByTitel(String titel);
 }
